7e1145e2c94a758d5feaf2bf7e0e58368850fa64
Xiao Dong <xiaodong.chicago@gmail.com>
2020-03-01 20:11:43 -0600

    Update and rename run_realistic.sh to runrealistic.sh

--- EMP/run_realistic.sh
+++
@@ -1,1 +0,0 @@
-./bin/hu 1 12345 ../HighUtilizer/Realistic/hu1.64.dat & ./bin/hu 2 12345 ../HighUtilizer/Realistic/hu2.64.dat
---
+++ EMP/runrealistic.sh
@@ -0,0 +1,2 @@
+./bin/hu 1 12345 ../HighUtilizer/Realistic/hu1.64.dat & ./bin/hu 2 12345 ../HighUtilizer/Realistic/hu2.64.dat
+./bin/ci2 1 12345 ../ComorbidityIndex/Realistic/cmbd1.64.dat & ./bin/ci2 2 12345 ../ComorbidityIndex/Realistic/cmbd2.64.dat
