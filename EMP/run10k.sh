74f8778143e8b8ccbc5c8b65705e4c21e95b3ed0
Xiao Dong <xiaodong.chicago@gmail.com>
2020-03-01 20:19:31 -0600

    Update run10k.sh

--- EMP/run10k.sh
+++ EMP/run10k.sh
@@ -1,1 +1,2 @@
+./bin/hu 1 12345 ../HighUtilizer/10K/hu1.64.dat & ./bin/hu 2 12345 ../HighUtilizer/10K/hu2.64.dat
 ./bin/ci2 1 12345 ../ComorbidityIndex/10K/cmbd1.64.dat & ./bin/ci2 2 12345 ../ComorbidityIndex/10K/cmbd2.64.dat
